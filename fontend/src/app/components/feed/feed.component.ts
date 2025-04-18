2f5c38cf5f7192428b3b5b88b15f65730697461b
sudhanshu1010 <sudhanshukumar1010@gmail.com>
2025-04-18 15:55:46 +0530

    Working on angular routes

--- fontend/src/app/components/feed/feed.component.ts
+++ fontend/src/app/components/feed/feed.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-feed',
+  standalone: true,
   imports: [],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css'
--- fontend/src/app/components/post-content/post-content.component.ts
+++ fontend/src/app/components/post-content/post-content.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-content',
+  standalone: true,
   imports: [],
   templateUrl: './post-content.component.html',
   styleUrl: './post-content.component.css'
