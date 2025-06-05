9483ef4a8a318eed5e4c0e197986c2d3544a7a37
Sudhanshu Kumar <69042848+sudhanshu1010@users.noreply.github.com>
2025-06-05 18:20:48 +0530

    Delete fontend directory

--- fontend/src/app/app.config.server.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
-
-export const config: ApplicationConfig = {
-  providers: [provideHttpClient()]
-};
