eb501c9178e9332e68c32a7284232e29b3fac297
Sudhanshu Kumar <69042848+sudhanshu1010@users.noreply.github.com>
2025-06-05 18:20:38 +0530

    Delete backend directory

--- backend/src/main/java/com/ih/innerhunch/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ih.innerhunch.repository;
-
-import com.ih.innerhunch.model.TestUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<TestUser, Integer> {}
