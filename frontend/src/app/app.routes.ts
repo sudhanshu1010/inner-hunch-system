d22b54aa85d503fc52e926d4ad51a5ccd2d9304d
sudhanshu1010 <sudhanshukumar1010@gmail.com>
2025-08-07 17:07:11 +0530

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,25 +1,17 @@
 import { Routes } from '@angular/router';
 import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
-//import { ArticlesContainerComponent } from './components/articles-container/articles-container.component';
 import { ArticlesDetailComponent } from './components/articles-detail/articles-detail.component';
 import { ArticlesListComponent } from './components/articles-list/articles-list.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    // { path: '', component: ComingSoonComponent },
-    // { 
-    //     path: 'articles', 
-    //     component: ArticlesContainerComponent, 
-    //     children: [
-    //         { path: '', component: ArticlesListComponent},
-    //         { path: ':slug', component: ArticlesDetailComponent}
-    //     ]
-    // },
-    // { path: '', redirectTo: 'articles', pathMatch: 'full' },
-    // { path: '**', redirectTo: '' }
-
-
     { path: '', redirectTo: 'coming-soon', pathMatch: 'full' },
     { path: 'coming-soon', component: ComingSoonComponent },
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'contact-us', component: ContactUsComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'articles', component: ArticlesListComponent },
     { path: 'articles/:slug', component: ArticlesDetailComponent },
     { path: '**', redirectTo: 'coming-soon' }
