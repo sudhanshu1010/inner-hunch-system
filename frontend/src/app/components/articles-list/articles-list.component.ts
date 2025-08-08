19a1fa72ceb3cfed434dfc3018aada40773cc600
sudhanshu1010 <sudhanshukumar1010@gmail.com>
2025-08-08 16:23:18 +0530

    Sanity checks and prgress bar done

--- frontend/src/app/components/articles-list/articles-list.component.ts
+++ frontend/src/app/components/articles-list/articles-list.component.ts
@@ -4,23 +4,32 @@ import { ArticlesService } from '../articles.service';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
-
+import { PreLoaderComponent } from '../pre-loader/pre-loader.component';
 
 @Component({
   selector: 'app-articles-list',
-  imports: [CommonModule, FooterComponent, NavbarComponent],
+  imports: [CommonModule,
+    FooterComponent,
+    NavbarComponent,
+    PreLoaderComponent
+  ],
   templateUrl: './articles-list.component.html',
   styleUrl: './articles-list.component.css'
 })
+
 export class ArticlesListComponent implements OnInit {
   articles: any[] = [];
-
-  constructor(private articleService: ArticlesService, private router: Router) {}
+  isLoading = true;
+  constructor(private articleService: ArticlesService, private router: Router) { }
 
   ngOnInit(): void {
     this.articleService.getArticles().subscribe(data => {
       this.articles = data;
     });
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
   }
 
   viewArticle(slug: string) {
